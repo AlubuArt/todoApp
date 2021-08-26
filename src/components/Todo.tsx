d4a2715a60a30ba869d6737f0e764902669633fc
AlubuArt <jc@jcvisueldesign.dk>
2021-08-26 20:09:06 +0200

    css done for now

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import styles from "../views/Todos.module.css";
+import styles from "./Todo.module.css";
 import { TodoContext } from "../contexts/todosContext";
 import { Moment } from "moment";
 import DateAndTimePicker from "./DateAndTimePicker";
@@ -21,20 +21,23 @@ const Todo: React.FC<TodoProps> = ({ todo, id }, key) => {
   };
 
   return (
-    <div key={key} className={styles.todo}>
-      <span style={{ flex: 1 }}>{todo.title}</span>
-      <span style={{ flex: 1 }}>{todo.status}</span>
-      <select defaultValue={todo.status} name="status" onChange={handleToggle}>
-        <option value={"Todo"}>Todo</option>
-        <option value={"Doing"}>Doing</option>
-        <option value={"Done"}>Done</option>
-      </select>
-      <div>
+    <div key={key} className={styles.todoContainer}>
+      <div className={styles.todoTitle}>
+        <h4>{todo.title}</h4>
+      </div>
+      <div className={styles.statusSelector}>
+        <select defaultValue={todo.status} name="status" onChange={handleToggle}>
+            <option value={"Todo"}>Todo</option>
+            <option value={"Doing"}>Doing</option>
+            <option value={"Done"}>Done</option>
+        </select>
+      </div>
+      <div className={styles.dateAndTimePicker}>
         <DateAndTimePicker todo={todo} id={id} />
       </div>
-      <span style={{ cursor: "pointer" }} onClick={() => removeTodo(todo, id)}>
-        &times;
-      </span>
+      <div className={styles.deleteTodoButton}>
+        <button onClick={() => removeTodo(todo, id)}>Delete</button>
+      </div>
     </div>
   );
 };
